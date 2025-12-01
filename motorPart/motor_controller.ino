4a7d5702d4d3a0c0956a85ab56c7753e278eac76
sanghyeok <seosanghyuk0@gmail.com>
2025-12-01 18:16:36 +0900

    firmware modified

--- motorPart/motor_controller.ino
+++ motorPart/motor_controller.ino
@@ -15,18 +15,17 @@
 
 #define MOTOR_A_IN1 9
 #define MOTOR_A_IN2 10
-
 #define MOTOR_B_IN1 11
 #define MOTOR_B_IN2 12 
 
 #define ENABLE_A 3
 #define ENABLE_B 5
 // 초음파 센서 
-#define TRIG_PIN 7  
+#define TRIG_PIN 7
 #define ECHO_PIN 8
 
-#define ENCODER_A_INT 3
-#define ENCODER_B_INT 1
+#define ENCODER_A_INT A0
+#define ENCODER_B_INT A1
 
 #define IMU_INT 2 
 
@@ -46,12 +45,6 @@ volatile float g_robot_theta = 0.0;
 
 MPU6050 mpu(Wire);
 
-typedef struct _FloatPacket {
-  float value;
-  uint8_t bytes[4];
-} FloatPacket_t;
-
-
 //--- 2. Queue 핸들 정의 ---
 QueueHandle_t xMotorQueue;
 
@@ -60,54 +53,41 @@ TaskHandle_t Motor_ESTOP;
 
 //--- 3. Queue로 보낼 데이터 타입 정의 ---
 typedef enum {
-  CMD_STOP,
-  CMD_UP,
-  CMD_DOWN,
-  CMD_LEFT,
-  CMD_RIGHT,
-  CMD_INVALID
+  CMD_STOP, CMD_UP, CMD_DOWN, CMD_LEFT, CMD_RIGHT, CMD_INVALID
 } MotorCommand_t; // MotorCommand_t 라는 새로운 타입을 만듦
 
 volatile MotorCommand_t g_currentMotorState = CMD_STOP;
 volatile bool g_isObstacleDetected = false; // E-STOP 상태 플래그
 volatile bool g_rx_error = false;
 volatile uint8_t g_robot_status = 0x01; // 0x01:대기/이동중, 0x08:도착완료
-/**
- * @brief MotorCommand_t enum 값을 문자열로 변환하는 헬퍼 함수
- * @param cmd 명령어 enum 값
- * @return const char* (문자열 포인터)
- */
 
-const char* getCommandString(MotorCommand_t cmd) {
-  switch (cmd) {
-    case CMD_STOP:   return "STOP";
-    case CMD_UP:     return "UP";
-    case CMD_DOWN:   return "DOWN";
-    case CMD_LEFT:   return "LEFT";
-    case CMD_RIGHT:  return "RIGHT";
-    default:         return "INVALID";
-  }
-}
+void rotate_sequence();
+void move_sequence(float target_cm = 10.0);
+void motor_speed(int spd);
+void Motor_UP();
+void Motor_DOWN(int spd = 100);
+void Motor_LEFT(int spd = 150);
+void Motor_RIGHT(int spd = 150);
+void Motor_STOP();
 
-void ISR_Encoder_A(){
-  g_ENCODER_LEFT_COUNT++;  
-}
+// PID
+double Input, Output, Setpoint;
+double Kp = 1.5, Ki = 0.01, Kd = 0.5; 
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+float g_target_heading_deg;
 
-void ISR_Encoder_B(){
-  g_ENCODER_RIGHT_COUNT++;
-}
+void ISR_Encoder_A(){ g_ENCODER_LEFT_COUNT++; }
+
+void ISR_Encoder_B(){ g_ENCODER_RIGHT_COUNT++; }
+
 void setup() {
   Serial.begin(115200);
   
-  pinMode(MOTOR_A_IN1, OUTPUT);
-  pinMode(MOTOR_A_IN2, OUTPUT);
-  pinMode(MOTOR_B_IN1, OUTPUT);
-  pinMode(MOTOR_B_IN2, OUTPUT);
+  pinMode(MOTOR_A_IN1, OUTPUT); pinMode(MOTOR_A_IN2, OUTPUT);
+  pinMode(MOTOR_B_IN1, OUTPUT); pinMode(MOTOR_B_IN2, OUTPUT);
 
-  pinMode(ENABLE_A, OUTPUT);
-  pinMode(ENABLE_B, OUTPUT);
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  pinMode(ENABLE_A, OUTPUT); pinMode(ENABLE_B, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT); pinMode(ECHO_PIN, INPUT);
 
   pinMode(ENCODER_A_INT, INPUT_PULLUP);
   pinMode(ENCODER_B_INT, INPUT_PULLUP);
@@ -116,14 +96,14 @@ void setup() {
 
   attachPCINT(digitalPinToPCINT(ENCODER_A_INT), ISR_Encoder_A, CHANGE);
   attachPCINT(digitalPinToPCINT(ENCODER_B_INT), ISR_Encoder_B, CHANGE);
-
+  
+  // NOISE PREVENTION
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
   //--- 1. 큐 생성 ---
   xMotorQueue = xQueueCreate(1, sizeof(MotorCommand_t));
 
-  if (xMotorQueue == NULL) {
-    //Serial.println("큐 생성 실패!");
-    while(1); // 시스템 정지
-  }
+  if (xMotorQueue == NULL) { while(1); } // System STOP 
 
   //--- 2. 태스크 생성 ---
   xTaskCreate(
@@ -145,15 +125,13 @@ void setup() {
   xTaskCreate(
     prvSensorandTX,
     "Sensor & TX",
-    128,
+    256,
     NULL,
     3,
     &Motor_ESTOP);
   
   vTaskStartScheduler();
 }
-
-
 
 void loop() {}
 
@@ -164,11 +142,11 @@ void prvRX(void *pvParameters) {
   MotorCommand_t cmd_to_send = CMD_INVALID; 
 
   for (;;) {
-    // 시리얼 포트에 읽을 데이터가 있는지 확인 (논블로킹)
     if (Serial.available() > 0) {
       rx_byte = (uint8_t)Serial.read();
-      
       uint8_t orientation = rx_byte & 0b00001111;
+
+      cmd_to_send = CMD_INVALID; // INVALID 일 때는 QUEUEOVERWRITE 안하기에 괜찮. 
 
       switch(orientation) {
         case 0x01:
@@ -203,30 +181,23 @@ void prvRX(void *pvParameters) {
          cmd_to_send = CMD_INVALID;
        }
       */
-      xQueueOverwrite(xMotorQueue, &cmd_to_send);
-      
+
       // ★ 여기가 핵심 ★
       if (cmd_to_send != CMD_INVALID) {
-         // 1. 정상 명령: 에러 끄고, 큐에 넣음
          g_rx_error = false; 
          xQueueOverwrite(xMotorQueue, &cmd_to_send);
       } else {
-         // 2. 이상한 명령: 에러 켜고, 큐에는 아무것도 안 넣음 (무시)
          g_rx_error = true;
       }
     }
-    // 이 태스크를 잠시(100ms) 재워서 다른 태스크(MotorTask)가 실행될 시간을 줌
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
-
-float g_target_heading_deg;
 
 void prvMotorTask(void *pvParameters) {
   (void) pvParameters;
   MotorCommand_t received_cmd;
  
-  // C언어 스타일의 무한 루프
   for (;;) {
     //--- 큐에서 명령이 올 때까지 무한정 대기 (Blocked 상태) ---
     // 큐에 데이터가 들어오면 이 태스크는 즉시 'Ready' 상태가 됨
@@ -257,7 +228,6 @@ void prvMotorTask(void *pvParameters) {
           break;
         case CMD_INVALID:
         default:
-          // 추후 protocol 작성 후 에러 값 표출 
           break;
       }
     }
@@ -277,7 +247,6 @@ void prvSensorandTX(void *pvParameters) {
   long prev_Encoder_B = 0;
   float prev_theta = 0; 
 
-  FloatPacket_t px, py, ptheta;
   uint8_t tx_byte = 0; 
 
   // portTICK_PERIOD_MS = 주기 / ms  50ms 
@@ -313,7 +282,6 @@ void prvSensorandTX(void *pvParameters) {
     float dist_center = (distance_A + distance_B) / 2.0;
 
     // Rotation 각도
-
     float current_angle_deg = mpu.getAngleZ();
     g_robot_theta = current_angle_deg * PI / 180;
     float avg_theta = (prev_theta + g_robot_theta) / 2.0;
@@ -328,8 +296,10 @@ void prvSensorandTX(void *pvParameters) {
     digitalWrite(TRIG_PIN, HIGH); delayMicroseconds(10); 
     digitalWrite(TRIG_PIN, LOW);
 
-    long duration = pulseIn(ECHO_PIN, HIGH, 5000);
+    long duration = pulseIn(ECHO_PIN, HIGH, 5000); // 없으면 0을 반환 
     int distance = duration / 58;
+
+    if (duration == 0) distance = 999; 
 
     bool isEmergency = distance > 0 && distance < STOP_DISTANCE_CM;
     if (isEmergency) {
@@ -339,40 +309,23 @@ void prvSensorandTX(void *pvParameters) {
     else {
       g_isObstacleDetected = false;  // 전역 플래그 해제
     }
-
-    //Serial.print("Heading: ");
-    //Serial.print(current_angle_deg, 1); // 소수점 첫째 자리까지 출력
-    
-    px.value = g_robot_x;
-    py.value = g_robot_y;
-    ptheta.value = g_robot_theta;
-
+  
     if (isEmergency) tx_byte = 0x02;
     else if (g_rx_error) tx_byte = 0x04;
     else tx_byte = g_robot_status; 
 
     Serial.write(0xAA);        // 설정을 안하면 1 byte 
     Serial.write(0xBB);
-    /*
-    Serial.write(px.bytes, sizeof(px.bytes)); // px.byte 를 4 byte 만큼 전송 
-    Serial.write(py.bytes, sizeof(px.bytes));
-    Serial.write(ptheta.bytes, sizeof(px.bytes));
-    */
+
     Serial.write(tx_byte);
 
     unsigned long endTime = micros();   // 2. 끝 시간 기록 -> Task Frequency 설정 위함 
     
     Serial.write((uint8_t*)&endTime, sizeof(endTime));
-
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
   }
 }
 
-// 전역 변수 (PID 제어 변수 선언)
-double Input, Output, Setpoint;
-// Kp, Ki, Kd 값은 실험을 통해 튜닝해야 함
-double Kp = 1.5, Ki = 0.01, Kd = 0.5; 
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void rotate_sequence() {
     Setpoint = g_target_heading_deg;
@@ -384,7 +337,7 @@ void rotate_sequence() {
 
     while (fabs(g_target_heading_deg - mpu.getAngleZ()) > 5.0) {
         
-        // 3초 타임아웃 (무한루프 방지)
+        // 3초 타임아웃 (무한루프 방지) 이러면 돌다가 정확하지 못한 곳에서 멈춰서 이상한곳으로 가는거 아님 ? 
         if (millis() - start_time > 3000) break;
 
         Input = mpu.getAngleZ();
@@ -408,22 +361,34 @@ void move_sequence(float target_cm = 10.0) {
 
   // 2. 목표 펄스 계산 ( 10cm / 펄스당거리 )
   long target_pulses = (long)(target_cm / DIST_PER_PULSE);
-  long start_count = g_ENCODER_LEFT_COUNT; 
+  taskENTER_CRITICAL();
+  long start_L = g_ENCODER_LEFT_COUNT; 
+  long start_R = g_ENCODER_RIGHT_COUNT;
+  taskEXIT_CRITICAL();
+  long start_count = (start_L + start_R) / 2 ;
   unsigned long start_time = millis();
 
-  Motor_UP(180); 
+  Motor_UP(); 
 
   while (abs(g_ENCODER_LEFT_COUNT - start_count) < target_pulses) {
-      
-      // 1. E-STOP 체크
       if (g_isObstacleDetected) {
         Motor_STOP();
         return;
       }
-           
-      // 2. 타임아웃 체크 (예: 10초)
-      if (millis() - start_time > 10000) {
-        break; // 강제 탈출
+
+      if (millis() - start_time > 5000) {
+        break; 
+      }
+      // 3. 현재 이동 거리 계산 (현재 평균 - 시작 평균)
+      long curr_L, curr_R;
+      taskENTER_CRITICAL();
+      curr_L = g_ENCODER_LEFT_COUNT;
+      curr_R = g_ENCODER_RIGHT_COUNT;
+      taskEXIT_CRITICAL();
+      long curr_avg = (curr_L + curr_R) / 2;
+      // 목표 도달 체크
+      if (abs(curr_avg - start_count) >= target_pulses) {
+          break;
       }
 
       vTaskDelay(10 / portTICK_PERIOD_MS); 
@@ -482,3 +447,5 @@ void Motor_DOWN() {
   digitalWrite(MOTOR_B_IN2, HIGH);
   motor_speed(150);
 }
+
+// 도착한 후 rotation theta 값 확인 후 rotate 완료 하고 ACK 신호 보내기. 
